4fabd0785067d8abffa8c247073e1faa24fdda19
syuwa-oisi <85724851+syuwa-oisi@users.noreply.github.com>
2021-09-11 17:52:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+import logo from './_assets/images/png/logo.png';
+import Header from './_components/Header_pc.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>こういうディレクトリ依存でファイル挿入とかする時</p>
+        <p>LateXくん触ってて良かった感あるね</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/_components/Header_pc.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Select from 'react-select';
+import HeaderLogo from '../_assets/images/png/Header_logo.png';
+import './Header_pc.css';
+import 'react-select-css/dist/react-select-css.min.css';
+
+const options = [
+  { value: 'america', label: 'america' },
+  { value: 'canada', label: 'canada' },
+  { value: 'china', label: 'china' },
+  { value: 'japan', label: 'japan' },
+  { value: 'korea', label: 'korea' },
+  { value: 'france', label: 'france' },
+  { value: 'spain', label: 'spain' },
+]
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        <div className='header-logo'>
+          <img src= {HeaderLogo} alt="Logo"/>
+        </div> 
+        <div className="check">                   
+          <Select options={options} />
+        </div>
+        <div className='searchbox'>
+          <Select isMulti name="pokemon" options={options}/>
+        </div>  
+      </div>
+    );
+  }
+}
+
+export default Header;
